d06cfdcb06039877aa259818981be9e238b5e800
Anthony Bocci <boccianthony@yahoo.fr>
2016-10-20 19:49:45 +0200

    Adds login method to connect without parameter

--- FTP.php
+++ FTP.php
@@ -48,7 +48,7 @@ class FTP
      * @param int $timeout the timeout during connections
      * @param bool $connect should it connect immediatly to host
      */
-    public function __construct($host, $port = 21, $timeout = 90, $connect = true)
+    public function __construct($host, $login = "", $password = "", $port = 21, $timeout = 90, $connect = true)
     {
         $this->host = $host;
         $this->port = $port;
@@ -68,21 +68,31 @@ class FTP
         return $this->connexionId !== false;
     }
 
+    /**
+     * Log on FTP server
+     * @return bool true if login is a success, false else
+     */
+    public function login()
+    {
+        return ftp_login($this->connexionId, $this->login, $this->password);
+    }
 
 
     /**
      * Call a ftp_* method without need to give connection stream
      * Example: $ftp->login($username, $password)
      * There is no need to write ftp_ nor give connexion stream
+     * Found at http://php.net/manual/en/book.ftp.php#105868
      * @param  callable $func The FTP function to call
      * @param  array $params The params to give to $func
      * @return mixed The value returned by $func
      */
-    public function __call($func,$params){
+    public function __call($func,$params)
+    {
         if(function_exists("ftp_" . $func)){
             array_unshift($params,$this->connexionId);
             return call_user_func_array("ftp_" . $func,$params);
-        }else{
+        } else{
             throw new \Exception("ftp_$func is not a valid FTP function");
         }
     }
